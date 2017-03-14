92966220daa8f3bd3aa59613d56fcd31c1fc4071
ViacheslavAfanasyev <sadney7@gmail.com>
2017-03-14 10:39:01 +0200

    Commit

---
+++ src/Sitecore.Support.126372/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Web
+{
+    public class Application : Sitecore.Web.Application
+    {
+        public virtual void Application_Start(object sender, EventArgs args)
+        {
+            Sitecore.DependencyInjection.ServiceLocator.MakeReadonly();
+        }
+    }
+}
---
+++ src/Sitecore.Support.126372/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.126372")]
+[assembly: AssemblyProduct("Sitecore.Support.126372")]
+[assembly: ComVisible(false)]
